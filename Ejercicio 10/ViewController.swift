97bd9cca265b5f3f30081128cfa8e49cf1acc486
Alex Alumno Mac <alejandro_ruiz_m-tsapp1ma2324@cev.com>
2024-12-03 08:47:27 +0000

    Initial Commit

---
+++ Ejercicio 10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio 10
+//
+//  Created by user231456 on 03/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
