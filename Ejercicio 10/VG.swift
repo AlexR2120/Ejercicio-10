9685cd004f45aa818120f04a16ccb932a1a203e3
Alex Alumno Mac <alejandro_ruiz_m-tsapp1ma2324@cev.com>
2024-12-03 10:10:59 +0000

    Ejercicio 10

---
+++ Ejercicio 10/VG.swift
@@ -0,0 +1,6 @@
+struct Reuniones {
+    var nombreSala: String
+    var nombreReserva: String
+    var personas: Int
+    var fecha: String
+}
--- Ejercicio 10/ViewController.swift
+++ Ejercicio 10/ViewController.swift
@@ -1,19 +1,60 @@
-//
-//  ViewController.swift
-//  Ejercicio 10
-//
-//  Created by user231456 on 03/12/2024.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    // Array para almacenar las reservas
+    var salasReservadas: [Reuniones] = []
+
+    // Outlets para los campos de texto
+    @IBOutlet weak var reservasTableView: UITableView!
+    @IBOutlet weak var nombreSalaTextField: UITextField!
+    @IBOutlet weak var nombreReservaTextField: UITextField!
+    @IBOutlet weak var personasTextField: UITextField!
+    @IBOutlet weak var fechaTextField: UITextField!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            super.viewDidLoad()
+            reservasTableView.dataSource = self
+            reservasTableView.delegate = self
+        }
+        
+        // Acción para añadir una reserva
+        @IBAction func agregarReserva(_ sender: UIButton) {
+            guard let nombreSala = nombreSalaTextField.text, !nombreSala.isEmpty,
+                  let nombreReserva = nombreReservaTextField.text, !nombreReserva.isEmpty,
+                  let personasString = personasTextField.text, let personas = Int(personasString),
+                  let fecha = fechaTextField.text, !fecha.isEmpty else {
+                print("Por favor, completa todos los campos correctamente.")
+                return
+            }
+            
+            // Crear la reserva y añadirla al array
+            let nuevaReserva = Reuniones(nombreSala: nombreSala, nombreReserva: nombreReserva, personas: personas, fecha: fecha)
+            salasReservadas.append(nuevaReserva)
+            
+            // Recargar la tabla
+            reservasTableView.reloadData()
+            
+            // Limpiar los campos
+            nombreSalaTextField.text = ""
+            nombreReservaTextField.text = ""
+            personasTextField.text = ""
+            fechaTextField.text = ""
+        }
+        
+        // Métodos del UITableViewDataSource
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return salasReservadas.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ReservaCell", for: indexPath)
+            let reserva = salasReservadas[indexPath.row]
+            
+            // Configurar la celda con los datos de la reserva
+            cell.textLabel?.text = "Sala: \(reserva.nombreSala) - Fecha: \(reserva.fecha)"
+            cell.detailTextLabel?.text = "Reservado por \(reserva.nombreReserva) (\(reserva.personas) personas)"
+            
+            return cell
+        }
     }
-
-
-}
-
